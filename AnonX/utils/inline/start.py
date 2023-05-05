fb0042406186097a5881f704c201a76e8ec6ec9c
ALEXANDER <107063122+Alex11011@users.noreply.github.com>
2023-05-05 10:16:10 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -44,12 +44,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                text=" ᴍᴀɪɴᴛᴀɪɴᴇʀ ", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text=" sᴏᴜʀᴄᴇ ", url=config.UPSTREAM_REPO
             )
         ],
      ]
